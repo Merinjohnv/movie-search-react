d80300e7381bb8327757f5a57686f29e7c98c71f
Merin John <merinjohnv@gmail.com>
2026-04-08 13:27:08 +0530

    Added clear button feature

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [hasSearched, setHasSearched] = useState(false);
-  const API_KEY="7820d872";
+  const API_KEY = "7820d872";
   const fetchMovies = async () => {
     setLoading(true);
     setHasSearched(true);
 
-  const response = await fetch(
-    `https://www.omdbapi.com/?s=${search}&apikey=${API_KEY}`
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${search}&apikey=${API_KEY}`,
     );
     const data = await response.json();
 
@@ -24,38 +24,61 @@ function App() {
     <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>Movie Search App</h1>
 
-      <input
-      type="text"
-      placeholder="Search movies..."
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      onKeyDown={(e) => {
-        if (e.key === "Enter"){
-        fetchMovies();
-        }
-      }}
-      style={{ padding: "10px", width: "250px", marginRight: "10px" }}
-      />    
+      <div style={{ marginBottom: "20px" }}>
+        <input
+          type="text"
+          placeholder="Search movies..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              fetchMovies();
+            }
+          }}
+          style={{ padding: "10px", width: "250px" }}
+        />
 
-      <button 
-      onClick={fetchMovies}
-      style={{ padding: "10px 20px", cursor: "pointer" }}>
-        Search
-      </button>
+        {search && (
+          <button
+            onClick={() => {
+              setSearch("");
+              setMovies([]); 
+              setHasSearched(false); 
+            }}
+            style={{
+              marginLeft: "5px",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+          >
+            X
+          </button>
+        )}
+
+        <button
+          onClick={fetchMovies}
+          style={{
+            marginLeft: "10px",
+            padding: "10px 20px",
+            cursor: "pointer",
+          }}
+        >
+          Search
+        </button>
+      </div>
 
       {loading && <p>Loading...</p>}
-      {hasSearched && !loading && movies.length === 0 && (
-        <p>No movies found</p>
-      )}
-      
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 250px))",
-        gap: "20px",
-        justifyContent: "center",
-        marginTop: "20px"
-      }}>
+      {hasSearched && !loading && movies.length === 0 && <p>No movies found</p>}
 
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 250px))",
+          gap: "20px",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
         {movies.map((movie) => (
           <MovieCard key={movie.imdbID} movie={movie} />
         ))}
